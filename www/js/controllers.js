c9ffe4cde053ef84e0813835a7f4a6e85489867c
mousyhead <mousyhead@hotmail.it>
2016-01-30 17:03:33 +0100

    updated aboutus and contactus views with services

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -165,30 +165,30 @@ angular.module('conFusion.controllers', [])
 
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', 'baseURL', function($scope, menuFactory, corporateFactory, baseURL) {
                                         
-                        $scope.baseURL = baseURL;                                        
-                        $scope.leader = corporateFactory.get({id:3});
-                        $scope.showDish = false;
-                        $scope.message="Loading ...";
-                        $scope.dish = menuFactory.getDishes().get({id:0})
-                        .$promise.then(
-                            function(response){
-                                $scope.dish = response;
-                                $scope.showDish = true;
-                            },
-                            function(response) {
-                                $scope.message = "Error: "+response.status + " " + response.statusText;
-                            }
-                        );
-                        $scope.promotion = menuFactory.getPromotion().get({id:0});
-            
-                    }])
+            $scope.baseURL = baseURL;                                        
+            $scope.leader = corporateFactory.get({id:3});
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+            $scope.dish = menuFactory.getDishes().get({id:0})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
+            $scope.promotion = menuFactory.getPromotion().get({id:0});
 
-        .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
-            
-                    $scope.leaders = corporateFactory.query();
-                    console.log($scope.leaders);
-            
-                    }])
+        }])
+
+        .controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {            
+            $scope.baseURL = baseURL;
+            $scope.leaders = corporateFactory.query();
+            console.log($scope.leaders);
+    
+        }])
 
 
 
